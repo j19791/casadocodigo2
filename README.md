525cd73024afa1d70722ee72e2d7ce7b7f15e239
Jefferson Ribeiro de Medeiros <jeffrm@gmail.com>
2019-04-24 08:22:36 -0300

    Aula 2 - Criação de serviços Rest
    Json
    
    Content Negotiation

--- README.md
+++ README.md
@@ -3,5 +3,10 @@ Projeto prático do curso Spring MVC - Parte 2
 
 [Link do projeto inicial](https://s3.amazonaws.com/caelum-online-public/springmvc-2-integracao-cache-seguranca-e-templates/springmvc2-projeto-inicial.zip)
 
+### Content Negotiation
+[Acesso ao json do detalhe do produto]
+(http://localhost:8080/casadocodigo/produtos/detalhe/15.json)
+
+
 
 
--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -79,4 +80,15 @@ public class ProdutosController {
 	    modelAndView.addObject("produto", produto);
 	    return modelAndView;
 	}
+	
+	@ResponseBody //o retorno do método será o corpo da resposta da requisição.
+	//Atenção: Jackson ja configurado no projeto
+	@RequestMapping("{id}")
+	public Produto detalheJson(@PathVariable("id") Integer id){
+	    
+	    return dao.find(id);
+	    
+	    
+	}
+	
 }
